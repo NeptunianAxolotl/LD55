3bcf7a4253d8d1f24824ff2799f397d150dd5581
Liam Walker-Greenough <beacon515@gmail.com>
2024-04-14 23:50:45 +1000

    Replacement music handler implementation.

--- musicHandler.lua
+++ musicHandler.lua
@@ -1,81 +1,144 @@
+-- Custom music handler because Beacon515L is a madman
 
+local loopDefs = {
+    ["A"] =
+      {
+        ["path"] = "resources/sounds/music/LD55_-_LOOP_A.ogg",
+        ["tensionLow"] = -1,
+        ["tensionHigh"] = 1,
+        ["duration"] = 31.5
+    },
+    ["B"] =
+      {
+        ["path"] = "resources/sounds/music/LD55_-_LOOP_B.ogg",
+        ["tensionLow"] = 1,
+        ["tensionHigh"] = 2,
+        ["duration"] = 7.5
+    },
+      ["C"] =
+      {
+        ["path"] = "resources/sounds/music/LD55_-_LOOP_C.ogg",
+        ["tensionLow"] = 2,
+        ["tensionHigh"] = 3,
+        ["duration"] = 12
+    }
+  }
+  
+  local transitionDefs =
+  {
+      ["AB"] = {
+        ["path"] = "resources/sounds/music/LD55_-_A_TO_B.ogg",
+        ["duration"] = 3.0
+      },
+      ["BC"] = {
+        ["path"] = "resources/sounds/music/LD55_-_B_TO_C.ogg",
+        ["duration"] = 3.0
+      },
+      ["CA"] = {
+        ["path"] = "resources/sounds/music/LD55_-_C_TO_A.ogg",
+        ["duration"] = 6.0
+        }
+  }
 
-local SoundHandler = require("soundHandler")
-local soundFiles = util.LoadDefDirectory("resources/soundDefs")
+local activeTrack = "A"
+local alternateBank = false
+
+-- set this to cause the music logic to transition through tracks
+-- range is 0 through 3
+bgmTension = 0
 
 local self = {}
 local api = {}
 local cosmos
 
-local font = love.graphics.newFont(70)
+local musicBanks = {
+  ["A"] = {},
+  ["B"] = {}
+}
 
--- First eligible tracks are used as start music
-local trackData = require("defs/musicTracks")
-local trackList = trackData.list
+local bgmTimer = 0
 
-local currentTrack = {}
-local trackRunning = false
-local initialDelay = true
-local currentTrackRemaining = 1
-local trackParity = 1
-local playingSounds = {}
+function loadSounds()
+  for k, v in pairs(loopDefs) do
+      musicBanks["A"][k] = love.audio.newSource(v["path"],"static")
+      musicBanks["B"][k] = love.audio.newSource(v["path"],"static")
+  end
+  for k, v in pairs(transitionDefs) do
+      musicBanks["A"][k] = love.audio.newSource(v["path"],"static")
+      musicBanks["B"][k] = love.audio.newSource(v["path"],"static")
+  end
+  
+end
+
+function cueTrack(which)
+  activeTrack = which
+  musicBanks[alternateBank and "A" or "B"][activeTrack]:play()
+  alternateBank = not alternateBank
+  if (activeTrack:len() == 2) then
+    bgmTimer = transitionDefs[which]["duration"]
+  else
+    bgmTimer = loopDefs[which]["duration"]
+  end
+end
 
 function api.StopCurrentTrack(delay)
-	currentTrackRemaining = delay or 0
+	-- implement as mute, not stop
 end
 
 function api.SetCurrentTrackFadeTime(fadeTime)
-	if trackRunning then
-		for i = 1, #currentTrack do
-			SoundHandler.SetSoundFade(currentTrack[i].sound, false, 1/fadeTime)
-		end
-	end
+	-- implement as volume manipulation, not stop
 end
 
 function api.Update(dt)
-	if self.needDtReset then
-		Global.ResetMissingDt()
-		self.needDtReset = false
-	end
-	if initialDelay then
-		initialDelay = initialDelay - dt
-		if initialDelay < 0 then
-			initialDelay = false
-			self.needDtReset = true
-		else
-			return
-		end
-	end
-	currentTrackRemaining = (currentTrackRemaining or 0) - dt
-	if currentTrackRemaining < 0 then
-		if cosmos.MusicEnabled() then
-			if trackRunning then
-				for i = 1, #trackList do
-					SoundHandler.StopSound(currentTrack[i])
-				end
-			end
-			trackRunning = true
-			currentTrackRemaining = soundFiles[trackData.useAsDurationForAllTracks or currentTrack[1]].duration
-			for i = 1, #trackList do
-				playingSounds[i] = SoundHandler.PlaySound(trackList[i], false, 1, 1, false, false, (trackData.WantTrack(cosmos, i) and 1) or 0, true)
-			end
-		elseif trackRunning then
-			for i = 1, #trackList do
-				SoundHandler.StopSound(trackList[i])
-			end
-			trackRunning = false
-		end
-	elseif trackRunning then
-		for i = 1, #trackList do
-			playingSounds[i].want = ((trackData.WantTrack(cosmos, i) and 1) or 0)
-		end
-	end
+	bgmTimer = bgmTimer - dt
+  if bgmTimer < 0 then
+    bgmTimer = 0
+  end
+  if bgmTimer == 0 then
+    -- Transition case - cue the next loop based on tension value
+    if (activeTrack:len() == 2) then
+      for k, v in pairs(loopDefs) do
+        if bgmTension > v["tensionLow"] and bgmTension <= v["tensionHigh"] then
+          cueTrack(k)
+          break
+        end
+      end
+    -- Loop case - within limits of current loop
+    elseif bgmTension > loopDefs[activeTrack]["tensionLow"] and bgmTension <= loopDefs[activeTrack]["tensionHigh"] then
+        cueTrack(activeTrack)
+    -- Loop case - below limits of current loop
+    elseif bgmTension < loopDefs[activeTrack]["tensionLow"] then
+        if activeTrack == "B" or activeTrack == "C" then
+          cueTrack("CA");
+        else 
+          cueTrack(activeTrack);
+        end
+    -- Loop case(s) - above limits of current loop
+    elseif activeTrack == "A" then
+        cueTrack("AB");
+    elseif activeTrack == "B" then
+        cueTrack("BC");
+    else 
+        cueTrack(activeTrack);
+    end
+  end
 end
 
 function api.Initialize(newCosmos)
 	self = {}
 	cosmos = newCosmos
-	initialDelay = 0
+	loadSounds()
+  cueTrack("A")
+end
+
+
+-- DEBUGGING - DO NOT FOR THE LOVE OF GOD LEAVE THIS IN
+function love.keyreleased( key )
+   if key == "j" then
+      bgmTension = bgmTension + 1
+  elseif key == "k" then
+      bgmTension = bgmTension - 1
+   end
 end
 
 return api
