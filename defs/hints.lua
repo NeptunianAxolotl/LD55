9b11faaae00fb9c85d6b2f636de49a87be0df268
GoogleFrog <googl3frog@gmail.com>
2024-04-17 17:38:59 +1000

    Polish the hints, because I can.

--- defs/hints.lua
+++ defs/hints.lua
@@ -1,33 +1,58 @@
 
 local hints = {
-	[[Elemental essences may be used to upgrade your abilities.
+	[[Consume captured elementals to upgrade your abilities.
 
-Selecting an upgrade will free an elemental of each other type, so choose wisely.]],
-	[[Not ready for the full Summoning?
+Selecting an upgrade will release a captured elemental of each other type, so choose wisely.]],
+	[[Not ready for the full Summoning? The Sandbox, found in the Menu, can be used to refine your technique.
 
-The Sandbox found in the Menu can be used to refine your technique.]],
-	[[The placement of sigils determines your elemental affinity.
+Check the Grimoire for new information each time it is opened.]],
+	[[Sigils towards an edge of the summoning circle bias your affinity toward that element.
 
-Sigils towards an edge will predominately attract elements of that type.]],
-	[[Upgrade Lightning to unlock hexagons.
+Elemental affinity determines which type of elementals are summoned.]],
+	[[Reach Lightning Level 2 to start inscribing hexagons.
 
-Beware, hexagons summon a great number of elementals in the direction they are facing.]],
-	[[Inscribe a mix of triangles and squares.
+Hexagons focus your affinity and summon a burst of elementals. Use them to boost your abilities, but be careful!]],
+	[[Sigils lose power over time, and fade rapidly while actively draining essence from nearby elementals.
 
-Squares efficiently deplete the power of elementals, while triangles absorb them.]],
-	[[- The Grimoire can be toggled with the <Tab> and <C> keys.
-- Line/Circle can be toggled with <Space> and <X>.
-- The camera can be moved with WSAD or the Arrow Keys.]],
-	[[Assimilate Fire and Lightning to boost the power of your sigils. Reach Level 8 Lightning to learn the secret of octagons.
+Larger sigils last longer, as do sigils created by a mage more versed in Fire.]],
+	[[Squares are powerful, but cannot fully absorb elementals.
+
+Triangles are weaker, but are the only basic sigil capable of full absorption.
+
+A mix of sigils is best.]],
+	[[Reach Level 8 Lightning to learn the secret of octagons.
 
 Inscribe an octagon to complete The Summoning.]],
-	[[Elements can be set to automatically upgrade.
+	[[Press [Space] or [X] to toggle between drawing lines and circles.
 
-Beware, this still depletes the rest of your reserves. First come, first served.]],
-	[[Assimilate Water and Life to survive the ravages of The Summoning.]],
-	[[Assimilate Earth and Chalk to improve your ability to draw and maintain the required lines of power.]],
-	[[Assimilate Ice and Air to avoid elementals as your lure them into your sigils.]],
+If a line or circle already exists between two points, then the other shape will be drawn.]],
+	[[Move the view with the Arrow Keys or WSAD, as well as with the mouse.
 
+Mouse scrolling can be turned off with Toggle Edge Scroll in the Menu.]],
+	[[Click [Auto] to automatically upgrade an ability when able.
+
+Automatic upgrades still release one elemental of each other type. First come, first served.]],
+	[[Upgrade Water and Life to better weather the ravages of The Summoning.
+
+Sometimes, rushing through a pack of elementals to draw a vital line is neccessary.]],
+	[[Upgrade Ice and Air to avoid the lunges of elementals as your lure them into your sigils.
+
+Elementals move at increased speed the longer they chase you.]],
+	[[Upgrade Fire for more powerful sigils, and Lightning to maintain more at once.
+
+Squares are much better at draining elemental essence than triangles, and larger sigils last longer.]],
+	[[Upgrade Earth and Chalk to improve your ability to create and sustain circles and lines.
+
+Circles push nearby elementals away when inscribed.]],
+	[[Elementals become larger and more numerous over time.
+
+Reach Level 8 Lightning and inscribe an octagon before you are overwhelmed!]],
+	[[Lines that form the edge of at least one sigil fade much more slowly.
+
+Lines that were part of a sigil fade rapidly once all their sigils are gone.]],
+	[[Overlapping many sigils of the same type in one place is difficult.
+
+Specifically, a sigil is only created if it would not share a corner, at the same orientation, with an existing sigil.]],
 }
 
 return hints
